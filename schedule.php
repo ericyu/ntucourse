8dd90e2149414eb397b5a70b95442b4d8efe79a0
Eric Yu <ericyu@gmail.com>
2005-01-08 17:24:34 +0000

    Fix a bug in abscence of not selecting cou_cname in schedule.php. Fix a bug in futility of selecting fields in schedule_pout.php.

--- schedule.php
+++ schedule.php
@@ -93,7 +93,7 @@ if(!$csv) {
 
 $total_course = $total_credit = 0;
 
-$SelectedFieldsSQL = column_sql($SelectedFields, split(" ", "cou_code dpt_code class daytime credit"));
+$SelectedFieldsSQL = column_sql($SelectedFields, split(" ", "cou_cname cou_code dpt_code class daytime credit"));
 
 if(isset($COU)) {
 	$size = sizeof($COU);
