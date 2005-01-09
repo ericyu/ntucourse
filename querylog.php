1c11259885f150ebc63770adadacd7770449c5c9
Eric Yu <ericyu@gmail.com>
2005-01-09 16:25:04 +0000

    Improve layout and display.

--- querylog.php
+++ querylog.php
@@ -1,13 +1,16 @@
 <?
 require_once('include/query.inc.php');
-
 require('include/header.inc.php');
+
+$check1[] = 'dpt_choice';
+$AllFields['dpt_choice'] = '系所代碼';
+
+$check = array_merge($check1, $check2);
 
 $res = mysql_query('SELECT * FROM querylog', $dbh);
 
-$check = array_merge($check1, $check2);
 while($row = mysql_fetch_assoc($res)) {
-	$data = unserialize($row['query']);
+	$data = @unserialize($row['query']);
 	foreach($check as $c) {
 		if(!empty($data[$c])) {
 			foreach(preg_split("/[, ]+/", $data[$c]) as $cur) {
@@ -21,7 +24,14 @@ foreach($check as $chk) {
 	if(empty($stats[$chk]))
 		continue;
 	arsort($stats[$chk]);
-	echo "<td style='vertical-align: text-top;'>$chk<table border>";
+	$stats[$chk] = array_slice($stats[$chk], 0, 100);
+	echo "<td style='vertical-align: text-top; background-color: ";
+	echo (in_array($chk, $check1) ? '#eeffdd' : '#ddeeff').";'>";
+	if(preg_match('/^not_(.+)$/', $chk, $result))
+		echo '不包括'.$AllFields["$result[1]"];
+	else
+		echo '包括'.$AllFields[$chk];
+	echo '<table border="1">';
 	foreach($stats[$chk] as $k => $c)
 		echo "<tr><td>$k<td>$c";
 	echo '</table>';
