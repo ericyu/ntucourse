6a2cc514c53e6a841c4171e6fb45ed5e23fd76fb
Eric Yu <ericyu@gmail.com>
2005-02-23 05:30:50 +0000

    New version of GetDetail tool (in Perl, getdetail.pl). Remove old version (getdetail.php). Change detail.php to restore links to aca's page. Add SQL table schema for comment9x_x.

--- detail.php
+++ detail.php
@@ -21,18 +21,14 @@ foreach($pattern as $f => $p)
 $SelectedFields = array('dptname', 'ser_no', 'cou_cname', 'credit', 'year',
 'tea_cname', 'clsrom', 'daytime', 'mark', 'co_gmark');
 
-$query = "SELECT $se.".implode(",$se.", $SelectedFields).",comment,$se.cou_code".
+$query = "SELECT tea,$se.".implode(",$se.", $SelectedFields).",comment,$se.cou_code".
 " FROM `$se` NATURAL LEFT JOIN `comment$se` WHERE $se.cou_code = '$cou_code'".
 " AND $se.class = '$class' LIMIT 0 , 30 ";
-
 $result = mysql_query($query, $dbh);
 if(!$result)
 	die('此學期資料不存在');
 $size = mysql_num_rows($result);
-
 $table_info = mysql_fetch_array(mysql_query("show table status from $db like 'comment$se'"));
-
-//echo $query;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -47,31 +43,31 @@ $table_info = mysql_fetch_array(mysql_query("show table status from $db like 'co
 <body bgcolor="#FFFFFF">
 <?
 if($size != 0) {  
-	echo "<p>查詢教務處網頁上的課程大綱: ";
-	list($c1, $c2) = explode(' ', $cou_code);
-	foreach($info as $i => $loc) {
-		echo "<a href=\"$loc?COU_CODE_1=$c1&amp;COU_CODE_2=$c2&amp;CLASS_1=$class\">伺服器 $i</a>&nbsp;&nbsp;";
+	if(array_shift(array_keys($SEMESTERS)) == $se) {
+		$r = mysql_fetch_assoc($result);
+		$tea = $r['tea'];
+		mysql_data_seek($result, 0);
+		echo "<p>查詢教務處網頁上的課程大綱: ";
+		list($c1, $c2) = explode(' ', $cou_code);
+		foreach($info as $i => $loc)
+			echo "<a href=\"$loc?COU_CODE_1=$c1&amp;COU_CODE_2=$c2&amp;CLASS_1=$class&amp;tea=$tea\">伺服器 $i</a>&nbsp;&nbsp;";
 	}
-	echo '(此功能因教務處資料格式更改，暫時無法使用)';
 
-	echo "<p>學期: $se<br>";
-	echo "課號: $cou_code<br>";
-	echo "班次: $class";
-	echo '<p><table border="1">';
+	echo "<p>學期: $se<br>課號: $cou_code<br>班次: $class";
+	echo '<p><table border="1"><tr>';
 
-	echo "<tr>";
-	foreach($SelectedFields as $s) {
+	foreach($SelectedFields as $s)
 		echo '<th>' . preg_replace("/<br>/", '', $AllFields[$s]);
-	}
 
 	while($row = mysql_fetch_assoc($result)) {
 		displayRow($row, $se, false, true, true);
 		$comment = $row['comment'];
 	}
 	echo "</table>";
-
-	echo "<p>本地端備份資料: (最後更新日期 $table_info[Update_time]. 可能較舊, 請再次確認)";
-	echo '<p><div style="padding: 20px; background: #fffcd8; border: 3px solid #ccc; width: 80%;">';
+?>
+<p>本地端備份資料: (最後更新日期 <?=$table_info['Update_time']?>. 可能較舊, 請再次確認)
+<p><div style="padding: 20px; background: #fffcd8; border: 3px solid #ccc; width: 80%;">
+<?
 	if(preg_match("/^http:\/\//", $comment))
 		echo "<a href=\"".urldecode($comment)."\">".urldecode($comment)."</a>";
 	else
---
+++ getdetail.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+use strict;
+use utf8;
+use Encode;
+use WWW::Mechanize;
+use Time::HiRes qw( usleep );
+use DBI;
+
+my $host = 'localhost';
+my $user = 'ntucourseupdate';
+my $password = 'courseupdate';
+my $db = 'ntucourse';
+my $table = '93_2';
+
+my @mech;
+for(0 ... 1) {
+	$mech[$_] = WWW::Mechanize->new();
+	$mech[$_]->requests_redirectable([]);
+}
+
+$mech[0]->get('http://investea.aca.ntu.edu.tw/course/11.htm');
+$mech[1]->get('http://couweb3.aca.ntu.edu.tw/course/11.htm');
+
+my $dbh = DBI->connect("DBI:mysql:database=$db;host=$host",
+	$user, $password, {RaiseError => 1} );
+$dbh->do("SET NAMES 'utf8'");
+
+my $sth = $dbh->prepare("SELECT DISTINCT cou_code,class,MIN(ser_no) AS ".
+	"serial FROM $table WHERE ser_no != '' GROUP BY cou_code,class")
+	or die "Couldn't prepare statement: " . $dbh->errstr;
+$sth->execute;
+
+my $count = 0;
+while(my $res = $sth->fetchrow_hashref) {
+	print ($count."\n") if ($count % 100 == 0);
+	$count++;
+	my $r = int(rand(2));
+
+	$mech[$r]->submit_form( fields => { number => $res->{'serial'} } );
+	my $response = $mech[$r]->follow_link( url_regex => qr/ConQuery.asp/ );
+	my $url = $response->base;
+	my $redirect = $response->header('Location');
+	my $text;
+
+	if($redirect ne '') {
+		$redirect =~ s/^%20(http:\/\/)/\1/g;
+		$text = $redirect;
+	} else {
+		($text) = ( decode("big5", $mech[$r]->content()) =~
+			/<textarea rows=12 cols=80 wrap=physical>(.+)<\/textarea>/s );
+		$text =~ s/\r//g;
+	}
+
+	my ($tea) = ( $url =~ /CLASS_1=.*&tea=(\d*)/ );
+	my $ins = $dbh->prepare("INSERT INTO tp VALUES (?, ?, ?, ?)");
+	$ins->execute($res->{'cou_code'}, $res->{'class'}, $tea, $text);
+	$mech[$r]->back();
+	$mech[$r]->back();
+	usleep(500);
+}
+
+$dbh->disconnect;
+
