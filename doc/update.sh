81820423e9fc3d6c316d7edae5676c6b6929931e
Eric Yu <ericyu@gmail.com>
2012-02-09 12:26:08 +0000

    Fixed the output date for diff file name.

--- doc/update.sh
+++ doc/update.sh
@@ -7,7 +7,7 @@ mv ${FILE}.txt ..
 cd ..
 mysql -u ntucourseupdate -p ntucourse < course/doc/update.sql
 
-course/doc/diff.pl > public_html/dw/diffs/`date "+%m-%d"`.out
-course/doc/diff.pl > course/diffs/`date "+%m-%d"`.out
+course/doc/diff.pl > public_html/dw/diffs/`date "+%Y-%m-%d"`.out
+course/doc/diff.pl > course/diffs/`date "+%Y-%m-%d"`.out
 
 
