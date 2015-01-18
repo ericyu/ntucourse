8f25f5f54a94698794759886c4be48e69d4d6d8f
Eric Yu <ericyu@gmail.com>
2015-01-18 11:29:29 +0000

    Update to 103_2

--- UpdateData/fetchdata.pl
+++ UpdateData/fetchdata.pl
@@ -4,7 +4,7 @@ $sem = `cat CURRENT_SEMESTER`;
 chomp $sem;
 mkdir("tmp");
 chdir("tmp");
-system("wget -r -N -nH -nd -A .XLS,.xls ftp://ftp.ntu.edu.tw/NTU/course/");
+system("wget -r -N -nH --cut-dirs=2 -A .XLS,.xls ftp://ftp.ntu.edu.tw/NTU/course/");
 
 rename($_, uc) while(glob("COU*.*[Ss]"));
 @type = qw(01 02 03 04 05 06 07 08 09 10 0B 11 12 13 14 15 16 17 18 19 20);
